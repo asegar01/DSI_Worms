c44b4d14f429d84803735aef713a6d36a1e6c163
Javier V <javilleg@ucm.es>
2022-04-03 20:29:10 +0200

    tiedna

--- DSI_Worms/Game.xaml.cs
+++ DSI_Worms/Game.xaml.cs
@@ -25,6 +25,58 @@ namespace DSI_Worms
         public Game()
         {
             this.InitializeComponent();
+
+            StorePage.Visibility = Visibility.Collapsed;
+
+            Fuego.Visibility = Visibility.Visible;
+
+            CuerpoaCuerpo.IsChecked = false;
+            CaC.Visibility = Visibility.Collapsed;
+
+            Explosivos.IsChecked = false;
+            Explos.Visibility = Visibility.Collapsed;
+        }
+
+        private void Store_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            StorePage.Visibility = Visibility.Visible;
+        }
+        private void On_Back(object sender, TappedRoutedEventArgs e)
+        {
+            StorePage.Visibility = Visibility.Collapsed;
+        }
+
+        private void Defuego_Checked(object sender, RoutedEventArgs e)
+        {
+            Fuego.Visibility = Visibility.Visible;
+
+            CuerpoaCuerpo.IsChecked = false;
+            CaC.Visibility = Visibility.Collapsed;
+
+            Explosivos.IsChecked = false;
+            Explos.Visibility = Visibility.Collapsed;
+        }
+
+        private void CuerpoaCuerpo_Checked(object sender, RoutedEventArgs e)
+        {
+            Defuego.IsChecked = false;
+            Fuego.Visibility = Visibility.Collapsed;
+
+            CaC.Visibility = Visibility.Visible;
+
+            Explosivos.IsChecked = false;
+            Explos.Visibility = Visibility.Collapsed;
+        }
+
+        private void Explosivos_Checked(object sender, RoutedEventArgs e)
+        {
+            Defuego.IsChecked = false;
+            Fuego.Visibility = Visibility.Collapsed;
+
+            CuerpoaCuerpo.IsChecked = false;
+            CaC.Visibility = Visibility.Collapsed;
+
+            Explos.Visibility = Visibility.Visible;
         }
     }
 }
